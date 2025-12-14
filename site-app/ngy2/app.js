54293adcb4a28dd68db04157330eb2714bd554b0
deswald-dees <130974864+deswald-dees@users.noreply.github.com>
2025-12-14 13:07:05 +0000

    Update site-app/ngy2 load album implementation

--- site-app/ngy2/app.js
+++ site-app/ngy2/app.js
@@ -1,7 +1,13 @@
 document.addEventListener('alpine:init', () => {
   console.log('Alpine is ready!');
-  function loadAlbum(albumPath) {
+  function loadAlbum(albumId) {
+    const albumPath = 'albums/' + albumId + '.json';
     jQuery.getJSON(albumPath, function (data) {
+      //Update the global store value
+      Alpine.store('ngy').currentAlbum = albumId;
+      Alpine.store('ngy').currentAlbumTitle = data.name;
+
+      // Remap the json structure to fit the nano gallery object structure
       let nanoGalleryItems = jQuery.map(data.images, function (image, index) {
         return {
           src: image.src,
@@ -13,10 +19,25 @@ document.addEventListener('alpine:init', () => {
       jQuery("#nanogallery2").nanogallery2({
         // ### gallery settings ### 
         thumbnailHeight: 200, thumbnailWidth: 200,
+        thumbnailAlignment: 'scaled',
+        thumbnailGutterWidth: 0, thumbnailGutterHeight: 0,
+        thumbnailBorderHorizontal: 0, thumbnailBorderVertical: 0,
+
+        thumbnailToolbarImage: null,
+        thumbnailToolbarAlbum: null,
+        thumbnailLabel: { display: false },
+        thumbnailDisplayOrder: 'random',
+
+        // THUMBNAIL HOVER ANIMATION
+        thumbnailBuildInit2: 'image_scale_1.15',
+        thumbnailHoverEffect2: 'thumbnail_scale_1.00_1.05_300|image_scale_1.15_1.00',
+        touchAnimation: true,
+        touchAutoOpenDelay: 500,
+
         galleryMaxRows: 1,
         galleryDisplayMode: 'rows',
         gallerySorting: 'random',
-        thumbnailDisplayOrder: 'random',
+        
         galleryMosaic: [
           { "c": 1, "r": 1, "w": 2, "h": 2 },
           { "c": 3, "r": 1, "w": 1, "h": 1 },
@@ -25,8 +46,8 @@ document.addEventListener('alpine:init', () => {
           { "c": 5, "r": 1, "w": 1, "h": 1 },
           { "c": 4, "r": 2, "w": 2, "h": 1 },
         ],
-        galleryDisplayTransition: 'slideUp',
-        galleryDisplayTransitionDuration: 500,
+        galleryDisplayTransition: 'slideUp', galleryDisplayTransitionDuration: 500,
+
         // ### gallery content ### 
         items: nanoGalleryItems,
         locationHash: false
@@ -34,22 +55,24 @@ document.addEventListener('alpine:init', () => {
     });
   };
 
-  let defaultAlbum = 'people-western';
-  loadAlbum('albums/' + defaultAlbum + '.json');
+  let defaultAlbum = 'nature';
+  loadAlbum(defaultAlbum );
 
-  Alpine.data('AlbumSelector', function () {
-    return {
-      currentAlbum: defaultAlbum,
-      changeAlbum(event) {
-        // Determine the clicked element (use the passed event or fallback to the active element)
-        const _target = (event && event.target) || document.activeElement;
-        const _selectedAlbum = _target && _target.getAttribute ? _target.getAttribute('data-album-selector') : null;
-        if (!_selectedAlbum || _selectedAlbum ===this.currentAlbum) return;
+  Alpine.store('ngy', {
+    currentAlbum: defaultAlbum,
+    currentAlbumTitle: 'NONE',
+    changeAlbum(event) {
+      // Determine the clicked element (use the passed event or fallback to the active element)
+      const _target = (event && event.target) || document.activeElement;
+      const _selectedAlbum = _target && _target.getAttribute ? _target.getAttribute('data-album-selector') : null;
+      if (!_selectedAlbum || _selectedAlbum === Alpine.store('ngy').currentAlbum) return;
 
-        this.currentAlbum = _selectedAlbum;
-        jQuery('#nanogallery2').nanogallery2('destroy');
-        loadAlbum('albums/' + _selectedAlbum + '.json');
-      }
+      jQuery('#nanogallery2').nanogallery2('destroy');
+      loadAlbum(_selectedAlbum);
     }
+  });
+
+  Alpine.data('NanoGalleryContainer', function() {
+    return {}
   });
 });
